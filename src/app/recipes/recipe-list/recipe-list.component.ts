6eb58aac1d8a079a98051d001dde124a7f42698b
ElenaLj <elenaljubijankic@gmail.com>
2022-04-22 17:40:22 +0200

    add recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
 })
 
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Guacamole',
+      'Guacamole is an avocado-based dip, spread, or salad first developed in Mexico. In addition to its use in modern Mexican cuisine, it has become part of international and American cuisine as a dip, condiment and salad ingredient.',
+      'https://www.tribugolosa.com/cache/slideshow/43/f8/86/b8/a4-10.jpg/2cb6823c975ee09b0d93e071c71c86d5.jpg'
+      )
+  ];
 }
