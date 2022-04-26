2c82b81f8537bc8e572bbca52cc2308cc118d38b
ElenaLj <elenaljubijankic@gmail.com>
2022-04-26 10:47:47 +0200

    add recipes and ingredient model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,16 @@ export class RecipeListComponent {
       'Guacamole',
       'Guacamole is an avocado-based dip, spread, or salad first developed in Mexico. In addition to its use in modern Mexican cuisine, it has become part of international and American cuisine as a dip, condiment and salad ingredient.',
       'https://www.tribugolosa.com/cache/slideshow/43/f8/86/b8/a4-10.jpg/2cb6823c975ee09b0d93e071c71c86d5.jpg'
-      )
+    ),
+    new Recipe(
+      'Gnocchi cacio e pepe',
+      'Enjoy a comforting bowl of gnocchi with parmesan and black pepper. It\'s simple but utterly delicious, and the best part is, it only takes seven minutes to make',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-cacio-e-pepe-a1392fd.jpg'
+    ),
+    new Recipe(
+      'Thai Rice Noodle Salad',
+      'Just made it up 5 minutes ago and it\'s really good. It can be served warm or cold; toss again right before serving. Do not overcook the noodles.',
+      'https://itsnotcomplicatedrecipes.com/wp-content/uploads/2020/09/Thai-Vermicelli-Noodle-Salad-1.jpg'
+    )
   ];
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Gnocchi', 250),
+    new Ingredient('Noodles', 500)
+  ]
   constructor() { }
 
   ngOnInit(): void {
