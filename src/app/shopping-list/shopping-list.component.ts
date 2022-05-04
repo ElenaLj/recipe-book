3c3b256b2d28241e6c99ed7e5b4a5aa5a3c0e717
ElenaLj <elenaljubijankic@gmail.com>
2022-05-04 16:18:05 +0200

    add shopping list service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Gnocchi', 250),
-    new Ingredient('Noodles', 500)
-  ]
-  constructor() { }
+  ingredients: Ingredient[];
+  //   new Ingredient('Gnocchi', 250),
+  //   new Ingredient('Noodles', 500)
+  // ]
+  constructor(private shoppingListService: ShoppingListService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
   }
 
-  onIngredientAdded(ingredient: Ingredient){
-    this.ingredients.push(ingredient);
-  }
-
+  // onIngredientAdded(ingredient: Ingredient){
+  //   this.ingredients.push(ingredient);
+  // }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,20 @@
+import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Gnocchi', 250),
+    new Ingredient('Noodles', 500)
+  ];
 
+  // onIngredientAdded(ingredient: Ingredient){
+  //   this.ingredients.push(ingredient);
+  // }
+
+  //since we cannot access the same array we need this function to copy the same array
+  getIngredients(){
+    return this.ingredients.slice()
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
