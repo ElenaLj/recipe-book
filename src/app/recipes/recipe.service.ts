51cd2487ca9cb74b081c730122bc8fc68e5dd794
ElenaLj <e.ljubijankic@codermine.com>
2022-06-06 23:05:13 +0200

    WIP: fetching data with services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,45 +10,47 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Guacamole',
-      'Guacamole is an avocado-based dip, spread, or salad first developed in Mexico. In addition to its use in modern Mexican cuisine, it has become part of international and American cuisine as a dip, condiment and salad ingredient.',
-      'https://www.tribugolosa.com/cache/slideshow/43/f8/86/b8/a4-10.jpg/2cb6823c975ee09b0d93e071c71c86d5.jpg',
-      [
-        new Ingredient('Avocado', 3),
-        new Ingredient('Onion', 1),
-        new Ingredient('Cherry tomatoes', 200),
-        new Ingredient('Lime', 100),
+  private recipes: Recipe[] = [];
 
-      ]
-    ),
-    new Recipe(
-      'Gnocchi cacio e pepe',
-      'Enjoy a comforting bowl of gnocchi with parmesan and black pepper. It\'s simple but utterly delicious, and the best part is, it only takes seven minutes to make',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-cacio-e-pepe-a1392fd.jpg',
-      [
-        new Ingredient('Gnocchi', 250),
-        new Ingredient('Caciocavallo cheese', 100),
-        new Ingredient('Pepper', 10),
-        new Ingredient('Butter', 125)
-      ]
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Guacamole',
+  //     'Guacamole is an avocado-based dip, spread, or salad first developed in Mexico. In addition to its use in modern Mexican cuisine, it has become part of international and American cuisine as a dip, condiment and salad ingredient.',
+  //     'https://www.tribugolosa.com/cache/slideshow/43/f8/86/b8/a4-10.jpg/2cb6823c975ee09b0d93e071c71c86d5.jpg',
+  //     [
+  //       new Ingredient('Avocado', 3),
+  //       new Ingredient('Onion', 1),
+  //       new Ingredient('Cherry tomatoes', 200),
+  //       new Ingredient('Lime', 100),
 
-    ),
-    new Recipe(
-      'Thai Rice Noodle Salad',
-      'Just made it up 5 minutes ago and it\'s really good. It can be served warm or cold; toss again right before serving. Do not overcook the noodles.',
-      'https://itsnotcomplicatedrecipes.com/wp-content/uploads/2020/09/Thai-Vermicelli-Noodle-Salad-1.jpg',
-      [
-        new Ingredient('Rice noodles', 250),
-        new Ingredient('Carrots', 2),
-        new Ingredient('Onion', 1),
-        new Ingredient('Zucchini', 2),
-        new Ingredient('Bell pepper', 1),
-        new Ingredient('Soy sauce', 25),
-      ]
-    )
-  ];
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Gnocchi cacio e pepe',
+  //     'Enjoy a comforting bowl of gnocchi with parmesan and black pepper. It\'s simple but utterly delicious, and the best part is, it only takes seven minutes to make',
+  //     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-cacio-e-pepe-a1392fd.jpg',
+  //     [
+  //       new Ingredient('Gnocchi', 250),
+  //       new Ingredient('Caciocavallo cheese', 100),
+  //       new Ingredient('Pepper', 10),
+  //       new Ingredient('Butter', 125)
+  //     ]
+
+  //   ),
+  //   new Recipe(
+  //     'Thai Rice Noodle Salad',
+  //     'Just made it up 5 minutes ago and it\'s really good. It can be served warm or cold; toss again right before serving. Do not overcook the noodles.',
+  //     'https://itsnotcomplicatedrecipes.com/wp-content/uploads/2020/09/Thai-Vermicelli-Noodle-Salad-1.jpg',
+  //     [
+  //       new Ingredient('Rice noodles', 250),
+  //       new Ingredient('Carrots', 2),
+  //       new Ingredient('Onion', 1),
+  //       new Ingredient('Zucchini', 2),
+  //       new Ingredient('Bell pepper', 1),
+  //       new Ingredient('Soy sauce', 25),
+  //     ]
+  //   )
+  // ];
 
   constructor(private slService: ShoppingListService) { }
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map, tap } from 'rxjs/operators';
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -22,10 +23,21 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>('https://recipe-http-e2a57-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
-      .subscribe(recipes => {
-        this.recipeService.setRecipes(recipes)
-      })
+      .pipe(
+        // make sure ingredients is always set
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            }
+          })
+        }),
+        tap(recipes =>
+          this.recipeService.setRecipes(recipes)
+        )
+      )
   }
 }
