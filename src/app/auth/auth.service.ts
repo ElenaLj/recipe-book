23317415e5973a79dee5927596be301dae615652
ElenaLj <e.ljubijankic@codermine.com>
2022-07-14 11:57:53 +0200

    :construction: WIP login and auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
+import { environment } from "src/environments/environment";
 
 export interface AuthResponseData {
   idToken: string;
@@ -20,38 +21,48 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   signUp(email: string, password: string) {
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBbplZ7-CzgRAxbah8O0tApMI7VbsioxNI',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true
-      }).pipe(catchError(error => {
-        let errorMessage = 'An error occured!'
-        if (!error.error || !error.error.error) {
-          return throwError(errorMessage);
-        }
-
-        switch (error.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'This email has already been taken'
-            break;
-          case 'OPERATION_NOT_ALLOWED':
-            errorMessage = 'Password sign-in is disabled for this project'
-            break;
-          case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-            errorMessage = 'Too many attempts. Try later'
-            break;
-        }
-        return throwError(errorMessage);
-      }))
-  }
-
-  login(email: string, password: string) {
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBbplZ7-CzgRAxbah8O0tApMI7VbsioxNI',
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
       {
         email: email,
         password: password,
         returnSecureToken: true
       })
+      .pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An error occured!'
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(() => errorMessage);
+    }
+
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email has already been taken'
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password sign-in is disabled for this project'
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'Too many attempts. Try later'
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'No user with this email. Please sign up or try a different email'
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Ops try again. The password you inserted is wrong!'
+        break;
+    }
+    return throwError(() => errorMessage);
   }
 }
